272b736d32a5f21ebf2423608de9faeb458ba865
Nat3z <djanice0408@yahoo.com>
2022-11-26 22:15:46 -0800

    delete rss

--- src/pages/rss.xml.js
+++
@@ -1,9 +0,0 @@
-import rss from '@astrojs/rss';
-
-export const get = () =>
-	rss({
-		title: "nat3z.github.io",
-		description: "The website for @Nat3z",
-		site: import.meta.env.SITE,
-		items: import.meta.glob('./blog/**/*.{md,mdx}'),
-	});
